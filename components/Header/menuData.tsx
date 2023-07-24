619c88275e3b34dbe1a76e4afc49e2fff2c77d39
KumarAbhinav01 <abhinav.bitd@gmail.com>
2023-07-24 15:36:38 +0530

    Menu Update

--- components/Header/menuData.tsx
+++ components/Header/menuData.tsx
@@ -13,18 +13,44 @@ const menuData: Menu[] = [
     path: "/about",
     newTab: false,
   },
-  // {
-  //   id: 33,
-  //   title: "Blog",
-  //   path: "/blog",
-  //   newTab: false,
-  // },
   {
     id: 3,
     title: "Support",
     path: "/contact",
     newTab: false,
   },
+  {
+    id: 4,
+    title: "Demos",
+    newTab: true,
+    submenu: [
+      {
+        id: 41,
+        title: "Demo 1",
+        path: "https://campus-careline-demo1.vercel.app/",
+        newTab: true,
+      },
+      {
+        id: 42,
+        title: "Demo 2 (Upcoming)",
+        path: "",
+        newTab: false,
+      },
+      {
+        id: 43,
+        title: "Demo 3 (Upcoming)",
+        path: "",
+        newTab: false,
+      },
+    ]
+  },
+  // {
+  //   id: 33,
+  //   title: "Blog",
+  //   path: "/blog",
+  //   newTab: false,
+  // },
+  
   // {
   //   id: 4,
   //   title: "Pages",
